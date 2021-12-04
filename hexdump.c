523314347d531dbb28eac239ec33ce70b6d0ae8e
rbamb <1152575283@qq.com>
2021-12-05 01:59:05 +0800

    fix format

--- hexdump.c
+++ hexdump.c
@@ -5,61 +5,60 @@
 #include <string.h>
 
 static int width(int n) {
-	if (n == 0) {
-		return 1;
-	}
-	int res = 0;
-	while (n > 0) {
-		n /= 10;
-		res++;
-	}
-	return res;
+    if (n == 0) {
+        return 1;
+    }
+    int res = 0;
+    while (n > 0) {
+        n /= 10;
+        res++;
+    }
+    return res;
 }
 
 int hexdump(const void *buf, size_t len) {
-	if (buf == NULL && len == 0) {
-		return -1;
-	}
-	unsigned char *p = (unsigned char *)buf;
-	int row = 16;
-	int lines = len % row == 0 ? len / row : len / row + 1;
-	int line = 0;
-	int offset = width(lines);
-	char hex[3 * row + 1];
-	char ascii[row + 1];
-	memset(hex, 0, sizeof(hex));
-	memset(ascii, 0, sizeof(ascii));
-	for (unsigned int i = 0; i < len; i++) {
-		int idx = 3 * (i % row);
-		snprintf(&hex[idx], 4, "%02X ", p[i]);
-		if (p[i] >= ' ' && p[i] <= '~') {
-			ascii[i % row] = p[i];
-		} else {
-			ascii[i % row] = '.';
-		}
-		if (((i + 1) % row) == 0) {
-			printf("%s  %s\n", hex, ascii);
-			if (line % row == 0) {
-				putchar('\n');
-			}
-		}
-		if ((i % row) == 0) {
-			printf("%*.*d | ", offset, width(line), line);
-			line++;
-		}
-	}
-	int diff = len % row;
-	if (diff != 0) {
-		printf("%s ", hex);
-		int tot = (row - diff) * 3 - 1;
-		for (int i = 0; i < tot; i++) {
-			putchar(' ');
-		}
-		for (int i = diff; i < row; i++) {
-			ascii[i] = ' ';
-		}
-		printf("  %s\n", ascii);
-	}
-	return 0;
+    if (buf == NULL && len == 0) {
+        return -1;
+    }
+    unsigned char *p = (unsigned char *)buf;
+    int row = 16;
+    int lines = len % row == 0 ? len / row : len / row + 1;
+    int line = 0;
+    int offset = width(lines);
+    char hex[3 * row + 1];
+    char ascii[row + 1];
+    memset(hex, 0, sizeof(hex));
+    memset(ascii, 0, sizeof(ascii));
+    for (unsigned int i = 0; i < len; i++) {
+        int idx = 3 * (i % row);
+        snprintf(&hex[idx], 4, "%02X ", p[i]);
+        if (p[i] >= ' ' && p[i] <= '~') {
+            ascii[i % row] = p[i];
+        } else {
+            ascii[i % row] = '.';
+        }
+        if (((i + 1) % row) == 0) {
+            printf("%s  %s\n", hex, ascii);
+            if (line % row == 0) {
+                putchar('\n');
+            }
+        }
+        if ((i % row) == 0) {
+            printf("%*.*d | ", offset, width(line), line);
+            line++;
+        }
+    }
+    int diff = len % row;
+    if (diff != 0) {
+        printf("%s ", hex);
+        int tot = (row - diff) * 3 - 1;
+        for (int i = 0; i < tot; i++) {
+            putchar(' ');
+        }
+        for (int i = diff; i < row; i++) {
+            ascii[i] = ' ';
+        }
+        printf("  %s\n", ascii);
+    }
+    return 0;
 }
-
--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  int len = 128 + 4;
-  char s[len];
-  for (int i = 0; i < len; i++) {
-    s[i] = -10 + i;
-  }
-  if (hexdump((void *)s, sizeof(s)) == -1) {
-    fprintf(stderr, "hexdump() failed\n");
-  }
-  return 0;
+	int len = 128 + 4;
+	char s[len];
+	for (int i = 0; i < len; i++) {
+		s[i] = -10 + i;
+	}
+	if (hexdump((void *)s, sizeof(s)) == -1) {
+		fprintf(stderr, "hexdump() failed\n");
+	}
+	return 0;
 }
 
